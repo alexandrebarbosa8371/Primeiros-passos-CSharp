c600792e607d883a958f200455f884a9aecfaf8b
Alexandre Barbosa <alexandrebarbosa8371@gmail.com>
2025-12-04 10:19:50 -0300

    Estudo sobre vetores.

---
+++ Dev03/Dev03/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace Dev03 {
+    internal class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Dev03/Dev03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dev03 {
+    internal class Program {
+        static void Main(string[] args) {
+            //Console.WriteLine("De quantas pessoas você quer saber a média de altura?");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //double[] vect = new double[n];
+
+            //for (int i = 0; i < n; i++) {
+            //    vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //}
+
+            //double sum = 0.0;
+            //for (int i = 0; i < n; i++) {
+            //    sum += vect[i];
+            //}
+            //double avg = sum / n;
+            //Console.WriteLine("Altura média = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+           int n = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++) {
+                string name = Console.ReadLine();
+                double price = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"Price average = {avg:F2}");
+        }
+    }
+}
